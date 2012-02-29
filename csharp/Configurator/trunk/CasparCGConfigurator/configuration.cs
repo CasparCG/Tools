4edb5a5c2ecd183bab1b806e9a71b5c8cfc61857
tk-3 <tk-3@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-02-29 15:54:35 +0000

    Initial Import, corrected

---
+++ csharp/Configurator/trunk/CasparCGConfigurator/configuration.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using TKLib;
+using System.Xml.Serialization;
+
+namespace CasparCGConfigurator
+{
+    public class configuration : INotifyPropertyChanged
+    {
+        private PropertyChangeManager<configuration> propertyChanges;
+
+        private paths _paths;
+        private BindingList<channel> _channels;
+        private string _loglevel; // [trace|debug|info|warning|error]</log-level>
+        private Boolean _channelgrid;
+        private Boolean _blendmodes;
+        private Boolean _autodeinterlace;
+        private Boolean _autotranscode;
+        private int _pipelinetokens;
+
+        public configuration()
+        {
+            propertyChanges = new PropertyChangeManager<configuration>(this);
+            
+            paths = new paths();
+            loglevel = "trace";
+            channelgrid = false;
+            blendmodes = false;
+            autodeinterlace = true;
+            autotranscode = true;
+            pipelinetokens = 2;
+            channels = new BindingList<channel>();
+        }
+
+        public paths paths
+        {
+            get { return _paths; }
+            set { _paths = value; this.propertyChanges.NotifyChanged(x => x.paths); }
+        }
+
+        [XmlElement(ElementName = "log-level")]
+        public string loglevel
+        {
+            get { return _loglevel; }
+            set { _loglevel = value; this.propertyChanges.NotifyChanged(x => x.loglevel); }
+        }
+
+        [XmlElement(ElementName = "channel-grid")]
+        public Boolean channelgrid
+        {
+            get { return _channelgrid; }
+            set { _channelgrid = value; this.propertyChanges.NotifyChanged(x => x.channelgrid); }
+        }
+
+        [XmlElement(ElementName = "blend-modes")]
+        public Boolean blendmodes
+        {
+            get { return _blendmodes; }
+            set { _blendmodes = value; this.propertyChanges.NotifyChanged(x => x.blendmodes); }
+        }
+
+        [XmlElement(ElementName = "auto-deinterlace")]
+        public Boolean autodeinterlace
+        {
+            get { return _autodeinterlace; }
+            set { _autodeinterlace = value; this.propertyChanges.NotifyChanged(x => x.autodeinterlace); }
+        }
+
+        [XmlElement(ElementName = "auto-transcode")]
+        public Boolean autotranscode
+        {
+            get { return _autotranscode; }
+            set { _autotranscode = value; this.propertyChanges.NotifyChanged(x => x.autotranscode); }
+        }
+
+        [XmlElement(ElementName = "pipeline-tokens")]
+        public int pipelinetokens
+        {
+            get { return _pipelinetokens; }
+            set { _pipelinetokens = value; this.propertyChanges.NotifyChanged(x => x.pipelinetokens); }
+        }
+
+        public BindingList<channel> channels
+        {
+            get { return _channels; }
+            set { _channels = value; this.propertyChanges.NotifyChanged(x => x.channels); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { this.propertyChanges.AddHandler(value); }
+            remove { this.propertyChanges.RemoveHandler(value); }
+        }
+    }
+}
---
+++ csharp/Configurator/trunk/CasparCGConfigurator/consumers.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using TKLib;
+using System.Xml.Serialization;
+
+namespace CasparCGConfigurator
+{
+    public class decklinkConsumer : AConsumer, INotifyPropertyChanged
+    {
+        private PropertyChangeManager<decklinkConsumer> propertyChanges;
+
+        private int _device;
+        private Boolean _embeddedaudio;
+        private string _latency;
+        private string _keyer;
+        private Boolean _keyonly;
+        private int _bufferdepth;
+
+        public decklinkConsumer()
+        {
+            propertyChanges = new PropertyChangeManager<decklinkConsumer>(this);
+            device = 1;
+            embeddedaudio = false;
+            latency = "normal";
+            keyer = "external";
+            keyonly = false;
+            bufferdepth = 3;
+        }
+
+        public int device
+        {
+            get { return _device; }
+            set { _device = value; this.propertyChanges.NotifyChanged(x => x.device); }
+        }
+
+        [XmlElement(ElementName = "embedded-audio")]
+        public Boolean embeddedaudio
+        {
+            get { return _embeddedaudio; }
+            set { _embeddedaudio = value; this.propertyChanges.NotifyChanged(x => x.embeddedaudio); }
+        }
+
+        public string latency
+        {
+            get { return _latency; }
+            set { _latency = value; this.propertyChanges.NotifyChanged(x => x.latency); }
+        }
+
+        public string keyer
+        {
+            get { return _keyer; }
+            set { _keyer = value; this.propertyChanges.NotifyChanged(x => x.keyer); }
+        }
+
+        [XmlElement(ElementName = "key-only")]
+        public Boolean keyonly
+        {
+            get { return _keyonly; }
+            set { _keyonly = value; this.propertyChanges.NotifyChanged(x => x.keyonly); }
+        }
+
+        [XmlElement(ElementName = "buffer-depth")]
+        public int bufferdepth
+        {
+            get { return _bufferdepth; }
+            set { _bufferdepth = value; this.propertyChanges.NotifyChanged(x => x.bufferdepth); }
+        }
+
+        public override string ToString()
+        {
+            return "Decklink";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { this.propertyChanges.AddHandler(value); }
+            remove { this.propertyChanges.RemoveHandler(value); }
+        }
+    }
+
+    public class screenConsumer : AConsumer, INotifyPropertyChanged
+    {
+        private PropertyChangeManager<screenConsumer> propertyChanges;
+
+        private int _device;
+
+        public screenConsumer()
+        {
+            propertyChanges = new PropertyChangeManager<screenConsumer>(this);
+        }
+
+        public int device
+        {
+            get { return _device; }
+            set { _device = value; this.propertyChanges.NotifyChanged(x => x.device); }
+        }
+
+        public override string ToString()
+        {
+            return "Screen";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { this.propertyChanges.AddHandler(value); }
+            remove { this.propertyChanges.RemoveHandler(value); }
+        }
+    }
+
+    [XmlInclude(typeof(decklinkConsumer))]
+    [XmlInclude(typeof(screenConsumer))]
+    public abstract class AConsumer : INotifyPropertyChanged
+    {
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+    }
+}
