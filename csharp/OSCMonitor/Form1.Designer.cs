1c1fbaa37a19b56bb7e6f6400a5908acd50ccea4
Andy Mace <andy.mace@mediauk.net>
2013-04-16 08:28:38 +0100

    Added OSC Monitor Application to csharp folder.
    Useful App to recieve TCP OSC Messages. Changes for UDP for CCG2.0 to come

---
+++ csharp/OSCMonitor/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace OSCMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btn = new System.Windows.Forms.Button();
+            this.cb1host = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(143, 8);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(76, 28);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Connect";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // cb1host
+            // 
+            this.cb1host.FormattingEnabled = true;
+            this.cb1host.Items.AddRange(new object[] {
+            "127.0.0.1"});
+            this.cb1host.Location = new System.Drawing.Point(12, 12);
+            this.cb1host.Name = "cb1host";
+            this.cb1host.Size = new System.Drawing.Size(125, 21);
+            this.cb1host.TabIndex = 2;
+            this.cb1host.Text = "127.0.0.1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 47);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(592, 429);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(293, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CasparCG OSC Monitor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(301, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Andy Mace (andy.mace@mediauk.net)";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 454);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(592, 22);
+            this.statusStrip1.TabIndex = 6;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 476);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cb1host);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OSCMonitor";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.ComboBox cb1host;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ csharp/OSCMonitor/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Bespoke.Common.Osc;
+using System.Net;
+using System.Collections;
+
+namespace OSCMonitor
+{
+    public partial class Form1 : Form
+    {
+        OscServer osc_server;
+        String ipaddress;
+        ArrayList oscmessages = new ArrayList();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            osc_server = new OscServer(Bespoke.Common.Net.TransportType.Tcp, IPAddress.Parse("127.0.0.1"), 5253);
+            osc_server.MessageReceived += new OscMessageReceivedHandler(osc_server_MessageReceived);
+            
+            //Need to build in Connected/Disconnected/FailedConnect Event Handlers
+            osc_server.FilterRegisteredMethods = false;
+
+            dataGridView1.ColumnCount = 3;
+           
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (osc_server.IsRunning)
+            {
+                osc_server.Stop();
+                ipaddress = osc_server.IPAddress.ToString();
+                toolStripStatusLabel1.Text = "Not Connected: " + ipaddress;
+            }
+            else {
+                osc_server.Start();
+                toolStripStatusLabel1.Text = "Connected: " + ipaddress;
+            }
+
+        }
+
+        void osc_server_MessageReceived(object sender, OscMessageReceivedEventArgs e)
+        {
+            //make sure "value" doesnt already exist
+            if (!(oscmessages.Contains(e.Message.Address)))
+            {
+                //since we've made it this far we can add it
+                oscmessages.Add(e.Message.Address);
+
+                switch (e.Message.Data.Length)
+                {
+                    case 1:
+                        AddDataGridRow(new string[] { e.Message.Address, e.Message.Data[0].ToString() });
+                        break;
+                    case 2:
+                        AddDataGridRow(new string[] { e.Message.Address, e.Message.Data[0].ToString(), e.Message.Data[1].ToString() });
+                        break;
+                    default:
+                        break;
+                }
+             }
+            else
+            {
+                UpdateDataGridRow(e.Message.Address, e.Message.Data); 
+            }
+
+        }
+
+        delegate void AddDataGridDel(String[] info);
+        public void AddDataGridRow(String[] info)
+        {
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                dataGridView1.Rows.Add(info);
+            }));
+        }
+
+        delegate void UpdateDataGridDel(String message, object[] data);
+        public void UpdateDataGridRow(String message, object[] data)
+        {
+            int i;
+           
+            for (i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if ((String)dataGridView1.Rows[i].Cells[0].Value == message)
+                {
+
+                    this.Invoke(new MethodInvoker(delegate()
+                    {
+                        switch (data.Length)
+                        {
+                            case 1:
+                                dataGridView1.Rows[i].Cells[1].Value = data[0].ToString();
+                                break;
+                            case 2:
+                                dataGridView1.Rows[i].Cells[1].Value = data[0].ToString();
+                                dataGridView1.Rows[i].Cells[2].Value = data[1].ToString();
+                                break;
+                            default:
+                                break;
+                        }
+                    }));
+                                       
+                }
+            }
+                
+           
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            dataGridView1.Width = Form1.ActiveForm.Width;
+            dataGridView1.Height = Form1.ActiveForm.Height + 47;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
